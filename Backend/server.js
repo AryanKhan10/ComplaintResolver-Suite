836fb007471a0b1f0c0ac53358f733bfae1cd5f0
malikmajeed <malikmajeedzia@gmail.com>
2024-12-03 05:18:13 -0800

    server.js file modifed as per user.controller.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,14 @@ import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './db/db.js'
 dotenv.config()
-
+// import router from './router.js'
 const app = express()
 app.use(express.json());
 import auth from "./routes/auth.route.js"
 import cookieParser from "cookie-parser";
+import router from './routes/user.route.js'
 
-
+app.use('/user', router)
 app.use(cookieParser())
 
 app.use("/api/v1/auth", auth);
@@ -16,7 +17,7 @@ app.use("/api/v1/auth", auth);
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h2>")
 })
-
+app.use('/user', router)
 connectDB().then(() => {
     app.on("error", () => {
         console.log("Express app not able to talk: ", error)

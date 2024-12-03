1c42355bd2dbf7f00a52c000404462e8c81e6e2e
Hamdan Raza <hamdanraza309@gmail.com>
2024-12-03 23:09:37 +0500

    setup for Feedback Controller

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
 import complaintRoute from "./routes/complaint.route.js";
+import feedbackRoute from "./routes/feedback.route.js";
 import auth from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 // Routes
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/complaints", complaintRoute); // Complaints routes
+app.use("/api/v1/feedback", feedbackRoute); // Feedback routes
 
 // Home Route
 app.get("/", (req, res) => {

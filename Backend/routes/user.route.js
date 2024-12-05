db309572a0e7a27099fb1fbd89f19d43e31d7ac6
unknown <cs.aryan.uet@gmail.com>
2024-12-05 21:32:06 +0500

    updated user.contr

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import {
-    createUser, deleteUserById, getAllUsers, getUserById, updateUserById, updateUserPassword
-} from '../controllers/userController.js'; // Adjust path as needed
-
+     deleteUserById, getAllUsers, getUserById, updateUserById, updateUserPassword
+} from '../controllers/user.controller.js'; // Adjust path as needed
+import { auth } from '../middlewares/auth.middleware.js';
 const router = express.Router();
-router.post('/', createUser);
-router.get('/', getAllUsers); 
-router.get('/:id', getUserById); 
-router.delete('/:id', deleteUserById); 
-router.put('/:id', updateUserById);
-router.put('/account/:id', updateUserPassword);
+// router.post('/', createUser);
+router.get('/getAllUsers', getAllUsers); 
+router.get('/getUser/:id', getUserById); 
+router.delete('/deleteUser/:id', deleteUserById); 
+// router.put('updateUser/:id', updateUserById);
+router.put('/resetPass',auth, updateUserPassword);
 
 
 export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,16 @@ dotenv.config()
 const app = express()
 app.use(express.json());
 import auth from "./routes/auth.route.js"
+import complaint from "./routes/complaint.route.js"
+import user from "./routes/user.route.js"
 import cookieParser from "cookie-parser";
 
 
 app.use(cookieParser())
 
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/complaint", complaint);
+app.use("/api/v1/user", user);
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h2>")
